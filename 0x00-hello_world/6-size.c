dd97aede8339de39b023b206b01769a524bd93c2
cayob <courageharvoxx@gmail.com>
2022-12-08 17:33:13 -0800

    C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 # include <stdio.h>
 /**
  * main - Entry
- * Return: Always 0:
+ * Return: Always 0
  */
 int main(void)
 {
@@ -11,4 +11,4 @@ int main(void)
 	printf("size of a long long int: %1 byte(s)\n", sizeof(long long int));
 	printf("size of a float: %1 byte(s)\n", sizeof(float));
 		return (0);
-
+}
