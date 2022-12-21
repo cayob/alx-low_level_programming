d81287fdf2831497e9202bf308e9a7e00ecbac80
cayob <courageharvoxx@gmail.com>
2022-12-21 06:15:04 -0800

    pointers and arrays

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* *_strncat - concatenates two strings
+*@dest: string to print
+*@src: string origin
+* @n: max umber of bytes from src
+*Return: dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		;
+	}
+
+	for (j = 0; src[j] != '\0' && j < n; i++, j++)
+	{
+		dest[i] = src[j];
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
