dd37cda1eb1b133cd804192c47a9da958d9e74e1
cayob <courageharvoxx@gmail.com>
2022-12-21 06:23:24 -0800

    atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,10 +1,11 @@
-#include "main.h"
-/**
-* _atoi - convert a string to an integer.
-* @s: the string to be comverted
-*Return: The integer value of the comverted string.
-*/
+#include "holberton.h"
 
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ *
+ * Return: The integer value of the converted string.
+ */
 int _atoi(char *s)
 {
 	int sign = 1;
@@ -15,13 +16,14 @@ int _atoi(char *s)
 			sign *= -1;
 
 		else if (*s >= '0' && *s <= '9')
-
 			num = (num * 10) + (*s - '0');
 
 		else if (num > 0)
 			break;
-	}
-	while (*s++);
+
+	} while (*s++);
 
 	return (num * sign);
 }
+
+
